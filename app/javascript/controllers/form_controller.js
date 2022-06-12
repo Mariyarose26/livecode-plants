d330e17fa124b127f24c1d144639595b3c3d9fb3
Mariya Rose <mariel.jesm@gmail.com>
2022-06-12 12:31:42 +1000

    Fixed Packages

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["button", "form"];
-
+  connect() {
+    console.log("connected");
+  }
   display() {
     this.formTarget.classList.remove("d-none");
     this.buttonTarget.classList.add("d-none");
